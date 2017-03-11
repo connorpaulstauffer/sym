fea9613493f59820e2cf6812dc628b611f634148
Connor Stauffer <connor.paul.stauffer@gmail.com>
2017-03-11 09:46:18 -0800

    Apply basic laws

--- lib/sym.ex
+++ lib/sym.ex
@@ -2,18 +2,53 @@ defmodule Sym do
   
   @operators [:~, :&, :|, :->, :<->]
   
+  # p ∨ c ≡ p
+  def identity({ :|, [p, :c] }), do: p
+  # def identity({ :|, [:c, p] }), do: p
+  # p ∧ t ≡ p
+  def identity({ :&, [p, :t] }), do: p
+  # def identity({ :&, [:t, p] }), do: p
+  def identity(p), do: p
+  
+  # ∼∼p ≡ p
   def double_negation({ :~, { :~, p } }), do: p
   def double_negation(p), do: p
   
+  # p ∧ p ≡ p
   def idempotent({ :&, [p, p] }), do: p
+  # p ∨ p ≡ p
   def idempotent({ :|, [p, p] }), do: p
   def idempotent(p), do: p
   
+  #  p ↔ q ≡ (p → q) ∧ (q → p)
   def biconditional({ :<->, [p, q] }), do: 
     { :&, [{ :->, [p, q] }, { :->, [p, q] }] }
   def biconditional(p), do: p
   
+  # ∼p → ∼q ≡ q -> p
+  def negated_conditionals({ :->, [{ :~, p }, { :~, q }] }), do: { :->, [q, p] }
+  def negated_conditionals(p), do: p
   
+  # ∼(p → q) ≡ p ∧ ∼q
+  def negated_conditional({ :~, { :->, [p, q] } }), do { :&, [p, { :~, q }] }
+  def negated_conditional(p), do: p
+  
+  def conditional({ :->, [p, q] }), do: { :|, [{ :~, p }, q] }
+  def conditional(p), do: p
+  
+  # ∼(p ∨ q) ≡ ∼p ∧ ∼q
+  def demorgan({ :~, { :|, [p, q] } }), do: { :&, [{ :~, p }, { :~, q }] }
+  # ∼(p ∧ q) ≡ ∼p ∨ ∼q
+  def demorgan({ :~, { :&, [p, q] } }), do: { :|, [{ :~, p }, { :~, q }] }
+  def demorgan(p), do: p
+  
+  # (p ∧ q) ∨ (p ∧ r) ≡ p ∧ (q ∨ r)
+  def reverse_distributive({ :|, [{ :&, [p, q] }, { :&, [p, r] }] }), do:
+    { :&, [p, { :|, [q, r] } ] }
+  # (p ∨ q) ∧ (p ∨ r) ≡ p ∨ (q ∧ r)
+  def reverse_distributive({ :&, [{ :|, [p, q] }, { :|, [p, r] }] }), do:
+    { :|, [p, { :&, [q, r] } ] }
+  def reverse_distributive(p), do: p
   
 end
 
@@ -24,19 +59,19 @@ end
 # { :->, [:p, :q] }
 
 
+# identity
 # double negation
 # indempotent
 # if only if
 # reverse conditional negations
 # negated conditional
 # conditional def
-# reverse demorgan
+# demorgan
 # reverse distributive
-# associative to remove parens
+
 # commutative to order alphabetically
 
 # only used if necessary. not normal simplification
-# identity
 # universal bound
 # absorption
 
